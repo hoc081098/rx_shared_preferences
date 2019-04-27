93c9b6243cb4ecdbd92699705b94e38f1582a807
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2019-04-27 20:46:19 +0700

    Update rx_shared_preference_test.dart

--- test/rx_shared_preference_test.dart
+++ test/rx_shared_preference_test.dart
@@ -1,7 +1,3 @@
-// Copyright 2017 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:rx_shared_preference/rx_shared_preference.dart';
