3474470e3f720668cf07fba6f1f25711f9a4aad7
Petrus Hoc <hoc081098@gmail.com>
2019-08-09 23:05:46 +0700

    Fix example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Rx Shared Preference example',
+      title: 'Rx Shared Preferences example',
       theme: ThemeData.dark(),
       home: const MyHomePage(),
     );
