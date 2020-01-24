3d8d448cf2ccbfbe3ce9b96075009882ff932aca
Petrus Học <hoc081098@gmail.com>
2020-01-24 18:53:22 +0700

    Add tests

--- test/rx_shared_preferences_test.dart
+++ test/rx_shared_preferences_test.dart
@@ -1,10 +1,12 @@
 import 'like_shared_prefs_test.dart' as like_shared_prefs_test;
 import 'streams_test.dart' as streams_test;
+import 'logger/logger_adapter_test.dart' as logger_adapter_test;
 import 'logger/default_logger_test.dart' as default_logger_test;
 import 'model/key_and_value_test.dart' as key_and_value_test;
 
 void main() {
   default_logger_test.main();
+  logger_adapter_test.main();
   key_and_value_test.main();
   like_shared_prefs_test.main();
   streams_test.main();
