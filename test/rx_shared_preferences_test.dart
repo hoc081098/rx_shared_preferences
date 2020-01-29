7f59a5c65ed440c3a26afb1af081edde97f9674d
Petrus Học <hoc081098@gmail.com>
2020-01-29 18:37:58 +0700

    Add test: RxSharedPreferences.asserts

--- test/rx_shared_preferences_test.dart
+++ test/rx_shared_preferences_test.dart
@@ -1,3 +1,6 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:rx_shared_preferences/rx_shared_preferences.dart';
+
 import 'like_shared_prefs_test.dart' as like_shared_prefs_test;
 import 'logger/default_logger_test.dart' as default_logger_test;
 import 'logger/logger_adapter_test.dart' as logger_adapter_test;
@@ -6,6 +9,13 @@ import 'model/key_and_value_test.dart' as key_and_value_test;
 import 'streams_test.dart' as streams_test;
 
 void main() {
+  test('RxSharedPreferences.asserts', () {
+    expect(
+      () => RxSharedPreferences(null),
+      throwsAssertionError,
+    );
+  });
+
   // logger
   default_logger_test.main();
   logger_adapter_test.main();
