2f0de4c4786b67bb6cf11e18810be90e08492475
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2020-10-10 19:58:13 +0700

    doc

--- lib/src/config/global_config.dart
+++ lib/src/config/global_config.dart
@@ -1,6 +1,6 @@
 import '../../rx_shared_preferences.dart';
 
-/// Global config for default singleton or extension.
+/// Global configs for default singleton or extension.
 class RxSharedPreferencesConfigs {
   /// Config for logger.
   static Logger logger = const DefaultLogger();
