01ac542f0c59c12d5283035aa0343585388c1666
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2020-12-27 00:36:39 +0700

    ref

--- example/lib/loggers.dart
+++ example/lib/loggers.dart
@@ -13,20 +13,20 @@ class EmptyLogger extends LoggerAdapter {
 /// Implements all methods
 class CustomLogger implements Logger {
   @override
-  void doOnDataStream(KeyAndValue<dynamic> pair) {}
+  void doOnDataStream(KeyAndValue<dynamic, dynamic> pair) {}
 
   @override
   void doOnErrorStream(dynamic error, StackTrace stackTrace) =>
       print('Error: $error');
 
   @override
-  void keysChanged(Iterable<KeyAndValue<dynamic>> pairs) {}
+  void keysChanged(Iterable<KeyAndValue<dynamic, dynamic>> pairs) {}
 
   @override
-  void readValue(Type type, String key, dynamic value) {}
+  void readValue(Type type, Object key, dynamic value) {}
 
   @override
-  void writeValue(Type type, String key, dynamic value, bool writeResult) {}
+  void writeValue(Type type, Object key, dynamic value, bool writeResult) {}
 }
 
 /// Or using default logger
