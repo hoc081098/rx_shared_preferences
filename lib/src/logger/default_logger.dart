9434672a2fafa0baedbc0920ba846b9ae4c911cf
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2021-02-24 09:34:03 +0700

    better logger

--- lib/src/logger/default_logger.dart
+++ lib/src/logger/default_logger.dart
@@ -5,20 +5,29 @@ import '../../rx_shared_preferences.dart';
 /// Default [RxSharedPreferencesLogger]'s implementation, simply print to the console.
 class RxSharedPreferencesDefaultLogger extends DefaultLogger<String, void>
     implements RxSharedPreferencesLogger {
+  /// Default logger tag.
+  static const defaultTag = '⚡ RxSharedPreferences';
+
   /// Construct a [RxSharedPreferencesDefaultLogger].
-  const RxSharedPreferencesDefaultLogger();
+  const RxSharedPreferencesDefaultLogger(
+      {String tag = defaultTag, bool trimValueOutput = false})
+      : super(tag: tag, trimValueOutput: trimValueOutput);
 
   @override
   void logOther(LoggerEvent<String, void> event) {
-    const rightArrow = '→';
+    const rightArrow = DefaultLogger.rightArrow;
+    const downArrow = DefaultLogger.downArrow;
 
     if (event is ReloadSuccessEvent) {
-      print(' $rightArrow Reload');
-      print(event.pairs.map((p) => '    $rightArrow $p').join('\n'));
+      print('$tag $downArrow Reload success');
+      print(event.keyAndValues
+          .map((p) => '    $rightArrow ${keyAndValueToString(p)}')
+          .join('\n'));
       return;
     }
+
     if (event is ReloadFailureEvent) {
-      print(' $rightArrow Reload $rightArrow ${event.error}');
+      print('$tag $rightArrow Reload $rightArrow ${event.error}');
       return;
     }
 
--- lib/src/logger/logger.dart
+++ lib/src/logger/logger.dart
@@ -2,15 +2,16 @@ import 'package:rx_storage/rx_storage.dart'
     show Logger, LoggerEvent, RxStorageError, KeyAndValue;
 
 /// Log messages about operations (such as read, write, value change) and stream events.
+/// Must handle [ReloadSuccessEvent] and [ReloadFailureEvent].
 abstract class RxSharedPreferencesLogger extends Logger<String, void> {}
 
 /// Reload successfully.
 class ReloadSuccessEvent implements LoggerEvent<String, void> {
   /// A list containing all values after reload.
-  final List<KeyAndValue<String, Object?>> pairs;
+  final List<KeyAndValue<String, Object?>> keyAndValues;
 
   /// Construct a [ReloadSuccessEvent].
-  ReloadSuccessEvent(this.pairs);
+  ReloadSuccessEvent(this.keyAndValues);
 }
 
 /// Reload failed.
