cc7f9e2a24ca61124e37177ca6b7e86b26a33aa9
hoc081098@gmail.com <hoc081098@gmail.com>
2024-08-13 18:40:54 +0700

    include tests

--- test/rx_shared_preferences_test.dart
+++ test/rx_shared_preferences_test.dart
@@ -1,9 +1,15 @@
 import 'adapter_test.dart' as adapter_test;
+import 'async_streams_test.dart' as async_streams_test;
+import 'legacy_streams_test.dart' as legacy_streams_test;
 import 'like_shared_prefs_test.dart' as like_shared_prefs_test;
-import 'legacy_streams_test.dart' as streams_test;
+import 'with_cache_streams_test.dart' as with_cache_streams_test;
 
 void main() {
   adapter_test.main();
   like_shared_prefs_test.main();
-  streams_test.main();
+
+  legacy_streams_test.main();
+
+  async_streams_test.main();
+  with_cache_streams_test.main();
 }
